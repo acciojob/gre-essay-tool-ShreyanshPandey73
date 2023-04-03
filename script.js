d75a56ad632edcab1374d2c4333a1dbe54b61f9f
ShreyanshPandey73 <117717418+ShreyanshPandey73@users.noreply.github.com>
2023-04-03 22:36:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+//your code here
+function countWords() {
+  var text = document.getElementById('evaluatedText').value;
+  var wordCount = 0;
+  if (text.trim() !== '') {
+    var matches = text.match(/\b\w+\b/g);
+    wordCount = matches ? matches.length : 0;
+  }
+  document.getElementById('wordCount').textContent = wordCount;
+}
